2d01a7f3d94a13d7535d89fa496a8f1f84fdf10e
xopowo7788 <xopowo7788@gmail.com>
2026-08-24 22:12:56 +0900

    Update videos.js

--- videos.js
+++ videos.js
@@ -1,13 +1,13 @@
 const VIDEO_CONFIG = [
-  { title:"휴머노이드 안내", src:"assets/videos/humanoid.mp4", poster:"assets/images/img-001.png" },
+  { title:"휴머노이드 안내", src:"https://xopowo7788.github.io/WRC_Beijing-Robot/assets/videos/humanoid.mp4", poster:"assets/images/img-001.png" },
   { title:"로봇 댄스", src:"https://xopowo7788.github.io/WRC_Beijing-Robot/assets/videos/robotdance.mp4", poster:"assets/images/img-004.png" },
-  { title:"격투 로봇", src:"assets/videos/fight.mp4", poster:"assets/images/img-012.png" },
-  { title:"밴드 로봇", src:"band.mp4", poster:"assets/images/img-017.png" },
-  { title:"아인슈타인 로봇", src:"assets/videos/einstein.mp4", poster:"assets/images/img-015.png" },
-  { title:"헬스 체험", src:"assets/videos/health.mp4", poster:"assets/images/img-016.png" },
-  { title:"컴패니언", src:"companion.mp4", poster:"assets/images/img-009.png" },
-  { title:"편의점 로봇", src:"assets/videos/store.mp4", poster:"assets/images/img-022.png" },
-  { title:"GALBOT 안내 로봇", src:"assets/videos/galbot.mp4", poster:"assets/images/img-023.png" }
+  { title:"격투 로봇", src:"https://xopowo7788.github.io/WRC_Beijing-Robot/assets/videos/fight.mp4", poster:"assets/images/img-012.png" },
+  { title:"밴드 로봇", src:"https://xopowo7788.github.io/WRC_Beijing-Robot/band.mp4", poster:"assets/images/img-017.png" },
+  { title:"아인슈타인 로봇", src:"https://xopowo7788.github.io/WRC_Beijing-Robot/assets/videos/einstein.mp4", poster:"assets/images/img-015.png" },
+  { title:"헬스 체험", src:"https://xopowo7788.github.io/WRC_Beijing-Robot/assets/videos/health.mp4", poster:"assets/images/img-016.png" },
+  { title:"컴패니언", src:"https://xopowo7788.github.io/WRC_Beijing-Robot/companion.mp4", poster:"assets/images/img-009.png" },
+  { title:"편의점 로봇", src:"https://xopowo7788.github.io/WRC_Beijing-Robot/assets/videos/store.mp4", poster:"assets/images/img-022.png" },
+  { title:"GALBOT 안내 로봇", src:"https://xopowo7788.github.io/WRC_Beijing-Robot/assets/videos/galbot.mp4", poster:"assets/images/img-023.png" }
 ];
 
 function setupClickVideos() {
